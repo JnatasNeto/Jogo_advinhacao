077c82a98727a0ee8a113a461ed4c99a8aa92590
Jonatas Neto <97569300+JnatasNeto@users.noreply.github.com>
2022-07-10 17:46:10 -0300

    Add files via upload

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,10 +6,21 @@ print("********************************")
 print("Bem-vindo ao jogo de advinhação!")
 print("********************************")
 
-numero_secreto = round(random.random() * 100)
-total_de_tentativas = 3
+numero_secreto = round(random.random() * 101)
+total_de_tentativas = 0
 
+print("Qual o nível da dificuldade ? ", numero_secreto)
+print("[1] Fácil [2] Médio [3] difícil")
 
+pontos = 1000
+nivel = int(input("Defina um nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 3
 
 for rodada in range (1,total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
@@ -25,13 +36,21 @@ for rodada in range (1,total_de_tentativas + 1):
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
-    if(acertou):
-        print("Você acertou")
+    if (acertou):
+        print("Você acertou e fez {} pontos!".format(pontos))
         break
     else:
-        if(maior):
-            print("Você errou, o seu chute foi maior do que o número secreto.")
-        elif(menor):
-            print("Você errou, o seu chute foi menor do que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
+        if (maior):
+            print("O seu chute foi maior que o número secreto")
+            if (rodada == total_de_tentativas):
+                print("O número secreto era {}. Você fez {}".format(
+                    numero_secreto, pontos))
+        elif (menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+            if (rodada == total_de_tentativas):
+                print("O número secreto era {}. Você fez {}".format(
+                    numero_secreto, pontos))
 
-print("Fim do jogo")
+    print("Fim do jogo")
