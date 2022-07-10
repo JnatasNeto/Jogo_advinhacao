04fa5fdd03be37b8c536f4da47ff464349edda26
Jonatas Neto <97569300+JnatasNeto@users.noreply.github.com>
2022-07-10 16:53:56 -0300

    Add files via upload

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,9 +1,14 @@
+import random
+
+
+
 print("********************************")
 print("Bem-vindo ao jogo de advinhação!")
 print("********************************")
 
-numero_secreto = 42
+numero_secreto = round(random.random() * 100)
 total_de_tentativas = 3
+
 
 
 for rodada in range (1,total_de_tentativas + 1):
