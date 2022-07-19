d5e11872caec87b3746e8936019747174a78ed1a
Jonatas Neto <97569300+JnatasNeto@users.noreply.github.com>
2022-07-18 23:19:42 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,24 +1,13 @@
 import random
 
+
 def jogar():
-    print("********************************")
-    print("Bem-vindo ao jogo de forca!")
-    print("********************************")
 
-    arquivo = open("palavras.txt", "r")
-    palavras = []
-    for linha in arquivo:
-        linha = linha.strip()
-        palavras.append(linha)
 
-    arquivo.close()
+    imprime_mensagem_abertura()
+    palavra_secreta =  carrega_palavra_secreta()
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
-    numero = random.randrange(0, len(palavras))
-    palavra_secreta = palavras[numero].upper()
-
-    print(palavras)
-
-    letras_acertadas = ["_" for letra in palavra_secreta]
     enforcou = False
     acertou = False
     erros = 0
@@ -49,5 +38,28 @@ def jogar():
     else:
         print("Você perdeu.")
     print("Fim do jogo")
+
+
+def inicializa_letras_acertadas(palavras):
+    return["_" for letra in carrega_palavra_secreta()]
+
+def imprime_mensagem_abertura():
+    print("********************************")
+    print("Bem-vindo ao jogo de forca!")
+    print("********************************")
+
+def carrega_palavra_secreta():
+        arquivo = open("palavras.txt", "r")
+        palavras = []
+        for linha in arquivo:
+            linha = linha.strip()
+            palavras.append(linha)
+
+        arquivo.close()
+
+        numero = random.randrange(0, len(palavras))
+        palavra_secreta = palavras[numero].upper()
+        return palavra_secreta
+
 if(__name__ == "__main__"):
     jogar()
